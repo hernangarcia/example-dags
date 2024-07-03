76e2d0c928fe6f6562ebfe1e21bf00eace635847
Hernan Garcia <hernan.garcia@gmail.com>
2024-07-03 10:31:56 +0200

    change to @DAG @Task syntax

--- dags/dataset-conditional-expressions-sample.py
+++ dags/dataset-conditional-expressions-sample.py
@@ -1,5 +1,5 @@
-from airflow.decorators import dag
-from airflow.models.datasets import Dataset
+from airflow.decorators import dag, task
+from airflow.datasets import Dataset
 from pendulum import datetime
 
 trading_data_asia = Dataset("s3://trading/asia/data.parquet")
@@ -7,22 +7,30 @@ trading_data_europe = Dataset("s3://trading/europe/data.parquet")
 trading_data_americas = Dataset("s3://trading/americas/data.parquet")
 regulatory_updates = Dataset("s3://regulators/updates.json")
 
-
 @dag(
     dag_id='risk_management_trading_data',
-
     start_date=datetime(2023, 5, 1),
-
     schedule=((trading_data_asia | trading_data_europe | trading_data_americas) & regulatory_updates),
-
     catchup=False
 )
-
 def risk_management_pipeline():
+    @task
+    def risk_analysis():
+        # Task for risk analysis
+        ...
 
-    # Tasks for risk analysis, reporting, and notifications
+    @task
+    def reporting():
+        # Task for reporting
+        ...
 
-    ...
+    @task
+    def notifications():
+        # Task for notifications
+        ...
+
+    analysis = risk_analysis()
+    report = reporting()
+    notify = notifications()
 
 risk_management_pipeline()
-
