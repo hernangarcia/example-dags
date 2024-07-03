e8c66b172c230d24dbf44dbd69844325e2de36d3
Hernan Garcia <hernan.garcia@gmail.com>
2024-07-03 10:43:07 +0200

    Changed to @Task and @Dag

--- dags/auto_pause_sample.py
+++ dags/auto_pause_sample.py
@@ -1,19 +1,18 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 
+@task
 def failing_task():
     raise ValueError("This task is designed to fail")
 
-with DAG(
-    dag_id="example_dag_with_auto_pause",
+@dag(
+    dag_id="auto_pause",
     start_date=datetime(2023, 1, 1),
     schedule_interval=timedelta(minutes=1),  # Run every minute
     catchup=False,
     max_consecutive_failed_dag_runs=3,  # Set the maximum number of consecutive failed DAG runs
-) as dag:
+)
+def example_dag_with_auto_pause():
+    failing_task_instance = failing_task()
 
-    task = PythonOperator(
-        task_id="failing_task",
-        python_callable=failing_task,
-    )
+example_dag_with_auto_pause()
