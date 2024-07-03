3d2573751634c944e5161e3f49fac2215e653ed2
Hernan Garcia <hernan.garcia@gmail.com>
2024-07-03 10:47:21 +0200

    change to @Task and @Dag

--- dags/custom_name_DTM_sample.py
+++ dags/custom_name_DTM_sample.py
@@ -1,4 +1,4 @@
-from airflow import DAG
+from airflow.decorators import dag
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 
@@ -6,16 +6,17 @@ def process_data(data):
     # Perform data processing logic here
     print(f"Processing data: {data}")
 
-with DAG(
+@dag(
     dag_id="custom_task_mapping_example",
     start_date=datetime(2023, 1, 1),
     schedule_interval=None,
     catchup=False,
-) as dag:
-
-    added_values = PythonOperator.partial(
+)
+def custom_task_mapping_example():
+    mapped_processes = PythonOperator.partial(
         task_id="process_data_source",
         python_callable=process_data,
-        op_kwargs={"y": 10},        
         map_index_template="Processing source={{ task.op_args[0] }}",
     ).expand(op_args=[["source_a"], ["source_b"], ["source_c"]])
+
+custom_task_mapping_example()
